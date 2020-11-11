184a7071e228d43593c793c2572e28bcfb8174ac
Siebe Krijgsman <krijgsman@riscure.com>
2020-11-11 12:50:14 +0100

    traceset_v2: Added preset samples parameter

--- src/main/java/com/riscure/trs/parameter/TraceParameter.java
+++ src/main/java/com/riscure/trs/parameter/TraceParameter.java
@@ -4,6 +4,8 @@ package com.riscure.trs.parameter;
  * This interface represents a parameter that is used in the trace data or the trace set header
  */
 public interface TraceParameter {
+    String SAMPLES = "SAMPLES";
+
     /**
      * The number of values of this type in this parameter
      * @return the number of values of this type in this parameter
