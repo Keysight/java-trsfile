d2340bda63cbd924b88d19ecd9881675266fc447
Siebe Krijgsman <krijgsman@riscure.com>
2020-12-28 16:37:04 +0100

    Added possibility to infer the type enum from the implementing class

--- src/main/java/com/riscure/trs/enums/ParameterType.java
+++ src/main/java/com/riscure/trs/enums/ParameterType.java
@@ -1,20 +1,24 @@
 package com.riscure.trs.enums;
 
 public enum ParameterType {
-    BYTE (0x01, Byte.BYTES),
-    SHORT (0x02, Short.BYTES),
-    INT (0x04, Integer.BYTES),
-    FLOAT (0x14, Float.BYTES),
-    LONG (0x08, Long.BYTES),
-    DOUBLE (0x18, Double.BYTES),
-    STRING (0x20, Byte.BYTES);
+    BYTE (0x01, Byte.BYTES, Byte.class, byte[].class),
+    SHORT (0x02, Short.BYTES, Short.class, short[].class),
+    INT (0x04, Integer.BYTES, Integer.class, int[].class),
+    FLOAT (0x14, Float.BYTES, Float.class, float[].class),
+    LONG (0x08, Long.BYTES, Long.class, long[].class),
+    DOUBLE (0x18, Double.BYTES, Double.class, double[].class),
+    STRING (0x20, Byte.BYTES, String.class, String.class);
 
     private final byte value;
     private final int byteSize;
+    private final Class<?> cls;
+    private final Class<?> arrayCls;
 
-    ParameterType(int value, int byteSize) {
+    ParameterType(int value, int byteSize, Class<?> cls, Class<?> arrayCls) {
         this.value = (byte)value;
         this.byteSize = byteSize;
+        this.cls = cls;
+        this.arrayCls = arrayCls;
     }
 
     public byte getValue() {
@@ -32,5 +36,14 @@ public enum ParameterType {
             }
         }
         throw new IllegalArgumentException("Unknown parameter type: " + value);
+    }
+
+    public static ParameterType fromClass(Class<?> cls) {
+        for (ParameterType parameterType : ParameterType.values()) {
+            if (parameterType.cls.isAssignableFrom(cls) || parameterType.arrayCls.isAssignableFrom(cls)) {
+                return parameterType;
+            }
+        }
+        throw new IllegalArgumentException("Unknown parameter type: " + cls);
     }
 }
