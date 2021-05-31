cbe492e67514e2147f3156eca809f7c48e564335
Siebe Krijgsman <krijgsman@riscure.com>
2021-05-31 13:51:50 +0200

    #42: Apparently <tt> is not acceptable anymore (automatically generated based on the FilterOutputStream constructor javadoc)

--- src/main/java/com/riscure/trs/io/LittleEndianOutputStream.java
+++ src/main/java/com/riscure/trs/io/LittleEndianOutputStream.java
@@ -15,7 +15,7 @@ public class LittleEndianOutputStream extends FilterOutputStream implements Data
      * underlying output stream.
      *
      * @param out the underlying output stream to be assigned to
-     *            the field <tt>this.out</tt> for later use, or
+     *            the field <code>this.out</code> for later use, or
      *            <code>null</code> if this instance is to be
      *            created without an underlying stream.
      */
