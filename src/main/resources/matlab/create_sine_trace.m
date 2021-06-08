d3bf85a81fc0bdc63cc33c86441cc658302ca6b3
Siebe Krijgsman <krijgsman@riscure.com>
2021-06-08 09:01:03 +0200

    Updated MATLAB example scripts and example trace
    This will now work correctly after the string length bug was fixed

--- src/main/resources/matlab/create_sine_trace.m
+++ src/main/resources/matlab/create_sine_trace.m
@@ -1,38 +1,44 @@
-%% Constants
-fs = 10e3;                              % Sampling frequency
-f_base = 10;                            % Base frequency. Other signals will be a harmonic of this.
-tmax = 0.5;                             % produce `tmax` seconds of signal
-trs_file = 'C:/MATLAB/sine_wave.trs';   % The trs filename. The `./` is important here to make it a proper relative path!
-global_title = "Matlab export";
-local_title_template = "Hz sine wave";
-title_space = 64;
+function create_sine_trace()
+    %% Constants
+    fs = 10e3;                              % Sampling frequency
+    f_base = 10;                            % Base frequency. Other signals will be a harmonic of this.
+    tmax = 0.5;                             % produce `tmax` seconds of signal
+    trs_file = 'C:/MATLAB/sine_wave.trs';   % The trs filename
+    global_title = "Matlab export";
+    local_title_template = "Hz sine wave";
+    title_space = 64;
 
-%% Create time base
-t = linspace(0,tmax,fs*tmax);
+    %% Create time base
+    t = linspace(0,tmax,fs*tmax);
 
-%% Add Inspector classes to classpath (change path to the actual location)
-inspector_jar = 'C:/MATLAB/trsfile-${project.version}.jar';
-javaaddpath(inspector_jar);
+    %% Add Inspector classes to classpath (change path to the actual location)
+    inspector_jar = 'C:/MATLAB/trsfile-${project.version}.jar';
+    javaaddpath(inspector_jar);
 
-%% Create metadata and fill relevant fields
-metadata = javaObject('com.riscure.trs.TRSMetaData');
-gt = javaMethod('valueOf', 'com.riscure.trs.enums.TRSTag', 'GLOBAL_TITLE');
-ts = javaMethod('valueOf', 'com.riscure.trs.enums.TRSTag', 'TITLE_SPACE');
-sx = javaMethod('valueOf', 'com.riscure.trs.enums.TRSTag', 'SCALE_X');
-metadata.put(gt, global_title);
-metadata.put(ts, uint32(title_space));
-metadata.put(sx, single(1/fs));
+    %% Create metadata and fill relevant fields
+    metadata = javaObject('com.riscure.trs.TRSMetaData');
+    gt = javaMethod('valueOf', 'com.riscure.trs.enums.TRSTag', 'GLOBAL_TITLE');
+    ts = javaMethod('valueOf', 'com.riscure.trs.enums.TRSTag', 'TITLE_SPACE');
+    sx = javaMethod('valueOf', 'com.riscure.trs.enums.TRSTag', 'SCALE_X');
+    metadata.put(gt, global_title);
+    metadata.put(ts, uint32(title_space));
+    metadata.put(sx, single(1/fs));
 
-%% Create the new file
-traceset = javaMethod('create', 'com.riscure.trs.TraceSet', trs_file, metadata);
+    %% Create the new file
+    traceset = javaMethod('create', 'com.riscure.trs.TraceSet', trs_file, metadata);
 
-%% Add 10 traces with sine wave of 100..10 Hz
-for n = [1:10]
-    y = sin(2*pi*f_base*n*t);
-    title = strcat(num2str(f_base*n), local_title_template);
-    trace = javaObject('com.riscure.trs.Trace', title, [], y, fs);
-    traceset.add(trace);
+    %% Add 10 traces with sine wave of 100..10 Hz
+    %% Note that a warning will be displayed here for the first trace because we reserved more title space than needed
+    for n = [1:10]
+        y = sin(2*pi*f_base*n*t);
+        trace_title = strcat(num2str(f_base*n), local_title_template);
+        params = javaObject('com.riscure.trs.parameter.trace.TraceParameterMap');   % Empty TRS2 parameter map
+        trace = javaObject('com.riscure.trs.Trace', trace_title, y, params);
+        traceset.add(trace);
+    end
+
+    %% close file to store to disk.
+    traceset.close();
+    %% MATLAB doesn't release the file handle until we call fclose
+    fclose(trs_file);
 end
-
-%% close file to store to disk.
-traceset.close();
--- src/main/resources/matlab/plot_trace.m
+++ src/main/resources/matlab/plot_trace.m
@@ -1,19 +1,28 @@
-%% Add Inspector classes to classpath (change path to the actual location)
-inspector_jar = 'C:/MATLAB/trsfile-${project.version}.jar';
-javaaddpath(inspector_jar);
+function plot_trace()
+    %% Add Inspector classes to classpath (change path to the actual location)
+    inspector_jar = 'C:/MATLAB/trsfile-${project.version}.jar';
+    javaaddpath(inspector_jar);
 
-%% Open one of the example tracesets (change path if necessary)
-trs_file = 'C:/MATLAB/example.trs';
-traceset = javaMethod('open', 'com.riscure.trs.TraceSet', trs_file);
+    %% Open one of the example tracesets (change path if necessary)
+    trs_file = 'C:/MATLAB/example.trs';
+    traceset = javaMethod('open', 'com.riscure.trs.TraceSet', trs_file);
 
-%% Get the first trace and extract the plaintext/ciphertext data
-trace = traceset.get(0);
-data = trace.getData();
-data_as_string = reshape(dec2hex(abs(data)), 1, []);
+    %% Get the first trace and extract the plaintext/ciphertext data
+    trace = traceset.get(0);
+    params = trace.getParameters();
+    %% The example trace has two parameters: INPUT and OUTPUT
+    %% Calling params.toString() will print the whole structure of names and associated data
+    input = params.getByteArray("INPUT");
+    output = params.getByteArray("OUTPUT");
+    input_as_string = reshape(dec2hex(abs(input)), 1, []);
+    output_as_string = reshape(dec2hex(abs(output)), 1, []);
 
-%% plot the trace, use plain/ciphertext as title
-plot(trace.getSample());
-title(data_as_string);
+    %% plot the trace, use plain/ciphertext as title
+    plot(trace.getSample());
+    title(strcat("Input=",input_as_string,", Output=",output_as_string));
 
-%% close the traceset
-traceset.close();
+    %% close the traceset
+    traceset.close();
+    %% MATLAB doesn't release the file handle until we call fclose
+    fclose(trs_file);
+end
