84eb4fb9a5b086398bf47177389f6bc936dfbd7e
Siebe Krijgsman <siebe.krijgsman@keysight.com>
2026-01-30 16:04:12 +0100

    #77: Reset the trace counter in case we created a copy of another MetaData object

--- src/main/java/com/riscure/trs/WritableTraceSet.java
+++ src/main/java/com/riscure/trs/WritableTraceSet.java
@@ -55,6 +55,7 @@ public class WritableTraceSet extends TraceSet {
             metaData.put(TITLE_SPACE, titleLength, false);
             metaData.put(SAMPLE_CODING, trace.getPreferredCoding(), false);
             metaData.put(TRACE_PARAMETER_DEFINITIONS, TraceParameterDefinitionMap.createFrom(trace.getParameters()));
+            metaData.put(NUMBER_OF_TRACES, 0);
             TRSMetaDataUtils.writeTRSMetaData(writeStream, metaData);
             firstTrace = false;
         }
